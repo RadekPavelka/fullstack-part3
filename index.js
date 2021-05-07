19a2c9c52f21f59c4333ea88114e7ecef109c05b
Radek Pavelka <radekpavelka94@gmail.com>
2021-05-07 22:12:53 +0200

    uibuild

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
@@ -65,29 +67,19 @@ app.delete("/api/persons/:id", (request, response, next) => {
 })
 
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
-
-    if (!body.name) {
-        return response.status(400).json({
-            error: "name is missing"
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({
-            error: "number is missing"
-        })
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 
 })
 
@@ -99,8 +91,11 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
+            if (updatedPerson === null) {
+                response.status(404).end()
+            }
             response.json(updatedPerson)
         })
         .catch(error => next(error))
@@ -108,7 +103,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
+}
 
 app.use(unknownEndpoint)
 
